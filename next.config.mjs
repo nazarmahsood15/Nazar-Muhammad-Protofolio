128a39c4d5b3c628c2141966428424f9c63ac80c
Nazar Muhammad Khan Mahsood <nazarmahsood16@gmail.com>
2025-09-12 02:13:26 +0500

    fix: remove optimizeCss experimental flag

--- next.config.mjs
+++ next.config.mjs
@@ -18,9 +18,7 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
-  experimental: {
-    optimizeCss: true,
-  },
+ 
   reactStrictMode: true,
 
   // 🚀 Disable ESLint errors during build
